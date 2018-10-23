53deb1ffc6b7bd5cc58874efa84fcf276858d21c
Abdur Razzak <abdur.razzak@ctrends-software.com>
2018-10-24 00:52:15 +0600

    operator-&-variable.R updated

---
+++ operator-&-variable.R
@@ -0,0 +1,67 @@
+# Math Operator:
+
+# (+) Addition
+# (-) Subtraction
+# (*) Multiplication
+# (/) Division
+# (** or ^) power
+# -------------------------
+# Logical Operators:
+  
+# (==) Equal to 
+# (!=) Not equal to
+# (<) Less than
+# (>) Greater than
+# (<=) Less than or equal to
+# (>=) Greater than or equal to
+# (&) Logical AND
+# (|) Logical OR
+# is.na(x) missing value
+
+# Variables:
+
+# 1) Numeric Variable
+# 2) Character Variable
+
+2+3
+5**2
+5^3
+6/3
+
+a <- 5
+a
+b=10
+b
+15 -> c
+c
+b
+
+x<-y<-12
+x
+y
+b=20
+b
+class(a)
+is.numeric(b)
+
+# Character Variable
+ab<-'Dhaka'
+ab
+class(ab)
+is.character(ab)
+is.numeric(ab)
+
+# Logical Operators
+x<-3
+y<-7
+x > y
+x < y
+x > 10
+x < 100
+
+# List of variables
+ls()
+# Remove a variable
+rm(c)
+ls()
+c
